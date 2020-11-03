42f58ff8dfa299c0f3fae7d4a903e372b8004874
FacundoOs <osores.facundo@gmail.com>
2020-11-03 15:54:30 +0100

    Add test for user model. Add route for devise auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have valid Factory" do
+    expect(create(:user)).to be_valid
+  end
+
+  describe "Database table" do
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :tokens }
+  end
+
+  describe "Validations" do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to validate_presence_of :password }
+
+    context "should not have an invalid email address" do
+      emails = ["asdf@ ds.com", "@example.com", "test me @yahoo.com",
+                "asdf@example", "ddd@.d. .d", "ddd@.d"]
+
+      emails.each do |email|
+        it { is_expected.not_to allow_value(email).for(:email) }
+      end
+    end
+
+    context "should have a valid email address" do
+      emails = ["asdf@ds.com", "hello@example.uk", "test1234@yahoo.si",
+                "asdf@example.eu"]
+
+      emails.each do |email|
+        it { is_expected.to allow_value(email).for(:email) }
+      end
+    end
+  end
 end
