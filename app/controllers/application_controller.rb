0f87bebf2ff8e319c4e67d0fecbbec12fe7ecdc4
FacundoOs <osores.facundo@gmail.com>
2020-11-03 18:31:10 +0100

    configure permitted parameters in registration. Add test for the added parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:company_name, :email, :password, :password_confirmation, :company_url, :role]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,23 +20,5 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of :company_name }
     it { is_expected.to validate_presence_of :company_url }
     it { is_expected.to validate_presence_of :role }
-
-    context "should not have an invalid email address" do
-      emails = ["asdf@ ds.com", "@example.com", "test me @yahoo.com",
-                "asdf@example", "ddd@.d. .d", "ddd@.d"]
-
-      emails.each do |email|
-        it { is_expected.not_to allow_value(email).for(:email) }
-      end
-    end
-
-    context "should have a valid email address" do
-      emails = ["asdf@ds.com", "hello@example.uk", "test1234@yahoo.si",
-                "asdf@example.eu"]
-
-      emails.each do |email|
-        it { is_expected.to allow_value(email).for(:email) }
-      end
-    end
   end
 end
