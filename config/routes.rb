cf62c644249a8598c34276ebe0bc8b96eee5e76f
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-11-05 16:57:52 +0100

    Creates req spec for show action and adds route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for "User", at: "api/auth", skip: [:omniauth_callbacks]
   namespace :api do
-    resources :assignments, only: [:index, :create]
+    resources :assignments, only: [:index, :create, :show]
   end
 end
---
+++ spec/request/api/assignments/show_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe "POST /api/assignments", type: :request do
+  let(:user) { create(:user, role: "client") }
+  let(:credentials) { user.create_new_auth_token }
+  let(:assignment) { create(:assignment) }
+  let(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
+
+  describe "clients and develupers can see specific assignments" do
+    before do
+      get "/api/assignments/#{assignment.id}",
+          headers: headers
+    end
+    it "responds with ok status" do
+      expect(response).to have_http_status :ok
+    end
+
+    it "returns a specific assignment description" do
+      expect(response_json["assignment"]["description"]).to eq "MyText"
+    end
+  end
+end
