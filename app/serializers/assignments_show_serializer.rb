3972c7bc448e93da59d6d1c2267672b8c1a2adc5
FacundoOs <osores.facundo@gmail.com>
2020-11-09 09:15:48 +0100

    Add applicants to show serializer

--- app/serializers/assignments_show_serializer.rb
+++ app/serializers/assignments_show_serializer.rb
@@ -1,3 +1,3 @@
 class AssignmentsShowSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :skills, :budget, :points
+  attributes :id, :title, :description, :skills, :budget, :points, :applicants
 end
