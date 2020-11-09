1f8dfdd93b0a896ae96088a9a6b8f9c51b2bf3d4
sara <saralundkvist86@gmail.com>
2020-11-09 07:53:39 +0100

    removes stringinterpolation in spec

--- spec/request/api/assignments/update_spec.rb
+++ spec/request/api/assignments/update_spec.rb
@@ -1,58 +1,58 @@
-RSpec.describe "PUT /api/assignments", type: :request do
+RSpec.describe 'PUT /api/assignments', type: :request do
   let(:develuper) { create(:develuper) }
   let(:develuper2) { create(:develuper) }
   let(:credentials) { develuper.create_new_auth_token }
-  let(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
   let(:assignment) { create(:assignment) }
 
-  describe "develUper successfully apply to assignment" do
+  describe 'develUper successfully apply to assignment' do
     before do
       put "/api/assignments/#{assignment.id}",
           params: {
-            assignment: { applicants: "#{develuper.id}" },
+            assignment: { applicants: develuper.id.to_s }
           }, headers: headers
     end
 
     before do
       put "/api/assignments/#{assignment.id}",
           params: {
-            assignment: { applicants: "#{develuper2.id}" },
+            assignment: { applicants: develuper2.id.to_s }
           }, headers: headers
     end
 
-    it "responds with create status" do
+    it 'responds with create status' do
       expect(response).to have_http_status :ok
     end
 
-    it "returns success message" do
-      expect(response_json["message"]).to eq "successfully applied"
+    it 'returns success message' do
+      expect(response_json['message']).to eq 'successfully applied'
     end
 
-    it "updates an assignment with applicants" do
+    it 'updates an assignment with applicants' do
       assignment = Assignment.last
-      expect(assignment.applicants).to eq "[#{develuper.id}, #{develuper2.id}]"
+      expect(assignment.applicants).to eq [develuper.id, develuper2.id]
     end
   end
 
-  describe "unsuccessfully - develUper already applied to assignment" do
+  describe 'unsuccessfully - develUper already applied to assignment' do
     before do
       put "/api/assignments/#{assignment.id}",
           params: {
-            assignment: { applicants: "#{develuper.id}" },
+            assignment: { applicants: develuper.id.to_s }
           }, headers: headers
     end
     before do
       put "/api/assignments/#{assignment.id}",
           params: {
-            assignment: { applicants: "#{develuper.id}" },
+            assignment: { applicants: develuper.id.to_s }
           }, headers: headers
     end
-    it "responds with unprocessable_entity" do
+    it 'responds with unprocessable_entity' do
       expect(response).to have_http_status :unprocessable_entity
     end
 
-    it "returns success message" do
-      expect(response_json["message"]).to eq "You already applied to this assignment"
+    it 'returns success message' do
+      expect(response_json['message']).to eq 'You already applied to this assignment'
     end
   end
 end
